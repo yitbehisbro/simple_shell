dc280aacb2e9ecdd00cdb5127d43160793fbaebb
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-08-29 12:29:34 +0300

    Content added to README file

--- main.h
+++ main.h
@@ -1,6 +1,13 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 void lsh_loop(void);
 char **lsh_split_line(char *line);
 char *lsh_read_line(void);
