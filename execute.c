4402f96dc18c80766398fcfb7d05c0921d66e9a9
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-08-29 13:03:22 +0300

    Updates

--- execute.c
+++ execute.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/*
+  List of builtin commands, followed by their corresponding functions.
+ */
+char *builtin_str[] = {
+  "cd",
+  "help",
+  "exit"
+};
+
 int (*builtin_func[]) (char **) = {
   &lsh_cd,
   &lsh_help,
