00933dff7b06a3844a889879abfeac71969fbd3f
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-08-29 12:49:40 +0300

    updates

--- main.h
+++ main.h
@@ -8,21 +8,6 @@
 #include <stdio.h>
 #include <string.h>
 
-/*
-  List of builtin commands, followed by their corresponding functions.
- */
-char *builtin_str[] = {
-  "cd",
-  "help",
-  "exit"
-};
-
-int (*builtin_func[]) (char **) = {
-  &lsh_cd,
-  &lsh_help,
-  &lsh_exit
-};
-
 void lsh_loop(void);
 char **lsh_split_line(char *line);
 char *lsh_read_line(void);
