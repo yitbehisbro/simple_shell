68132d85e04bc93f7bf187701ed5206c378ef9b0
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-08-29 12:55:04 +0300

    Updates

--- builtin.c
+++ builtin.c
@@ -1,20 +1,5 @@
 #include "main.h"
 
-/*
-  List of builtin commands, followed by their corresponding functions.
- */
-char *builtin_str[] = {
-  "cd",
-  "help",
-  "exit"
-};
-
-int (*builtin_func[]) (char **) = {
-  &lsh_cd,
-  &lsh_help,
-  &lsh_exit
-};
-
 int lsh_num_builtins() {
   return sizeof(builtin_str) / sizeof(char *);
 }
