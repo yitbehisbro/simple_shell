ac1f5e6eb870737b3ec6052bd481599a4427da8f
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-08-29 12:40:48 +0300

    Updates to content

--- loop.c
+++ loop.c
@@ -1,19 +1,22 @@
+#include "main.h"
+
 /**
    @brief Loop getting input and executing it.
  */
 void lsh_loop(void)
 {
-  char *line;
-  char **args;
-  int status;
+	char *line;
+	char **args;
+	int status;
 
-  do {
-    printf("> ");
-    line = lsh_read_line();
-    args = lsh_split_line(line);
-    status = lsh_execute(args);
+	do
+	{
+		printf("$ ");
+		line = lsh_read_line();
+		args = lsh_split_line(line);
+		status = lsh_execute(args);
 
-    free(line);
-    free(args);
-  } while (status);
+		free(line);
+		free(args);
+	} while (status);
 }
