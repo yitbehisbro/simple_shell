08a62f7bb13b4044c0bb02f4985ed1ee73a724a0
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-08-30 11:22:58 +0300

    Updates

--- launch.c
+++ launch.c
@@ -17,7 +17,7 @@ int lsh_launch(char **args)
       //perror(args[0]);
       //perror(args[2]);
       fprintf(stderr, "not found\n");
-      exit(EXIT_FAILURE);
+      //exit(EXIT_FAILURE);
     }
     exit(EXIT_FAILURE);
   } else if (pid < 0) {
