f3ef12a6b97e9c3f7b883c20d5aebf40c7ec39db
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-08-31 12:38:58 +0300

    Updates

--- readline.c
+++ readline.c
@@ -1,58 +1,69 @@
 #include "main.h"
 
 /**
-   @brief Read a line of input from stdin.
-   @return The line from stdin.
+ * read_line - read a line
+ *
+ * Return: Null-terminated
  */
-char *lsh_read_line(void)
+char *read_line(void)
 {
-#ifdef LSH_USE_STD_GETLINE
-  char *line = NULL;
-  ssize_t bufsize = 0; // have getline allocate a buffer for us
-  if (getline(&line, &bufsize, stdin) == -1) {
-    if (feof(stdin)) {
-      exit(EXIT_SUCCESS);  // We received an EOF
-    } else  {
-      perror("lsh: getline\n");
-      exit(EXIT_FAILURE);
-    }
-  }
-  return line;
-#else
-#define LSH_RL_BUFSIZE 1024
-  int bufsize = LSH_RL_BUFSIZE;
-  int position = 0;
-  char *buffer = malloc(sizeof(char) * bufsize);
-  int c;
+	#ifdef LSH_USE_STD_GETLINE
 
-  if (!buffer) {
-    fprintf(stderr, "lsh: allocation error\n");
-    exit(EXIT_FAILURE);
-  }
+	char *line = NULL;
+	ssize_t bufsize = 0;
 
-  while (1) {
-    // Read a character
-    c = getchar();
+	if (getline(&line, &bufsize, stdin) == -1)
+	{
+		if (feof(stdin))
+		{
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			perror("getline\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+	return (line);
+	#else
+	#define LSH_RL_BUFSIZE 1024
+	int bufsize = LSH_RL_BUFSIZE;
+	int position = 0;
+	char *buffer = malloc(sizeof(char) * bufsize);
+	int c;
 
-    if (c == EOF) {
-      exit(EXIT_SUCCESS);
-    } else if (c == '\n') {
-      buffer[position] = '\0';
-      return buffer;
-    } else {
-      buffer[position] = c;
-    }
-    position++;
-
-    // If we have exceeded the buffer, reallocate.
-    if (position >= bufsize) {
-      bufsize += LSH_RL_BUFSIZE;
-      buffer = realloc(buffer, bufsize);
-      if (!buffer) {
-        fprintf(stderr, "lsh: allocation error\n");
-        exit(EXIT_FAILURE);
-      }
-    }
-  }
-#endif
+	if (!buffer)
+	{
+		fprintf(stderr, "allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+	while (1)
+	{
+		c = getchar();
+		if (c == EOF)
+		{
+			exit(EXIT_SUCCESS);
+		}
+		else if (c == '\n')
+		{
+			buffer[position] = '\0';
+			return (buffer);
+		}
+		else
+		{
+			buffer[position] = c;
+		}
+		position++;
+		if (position >= bufsize)
+		{
+			bufsize += LSH_RL_BUFSIZE;
+			buffer = realloc(buffer, bufsize);
+			if (!buffer)
+			{
+				fprintf(stderr, "allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+	#endif
 }
