71ee80737e94d7cb585ef5609c2fa693710cf7db
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-08-31 14:14:11 +0300

    Updates

--- split_line.c
+++ split_line.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM "\" \t\r\n\a"
 
 /**
  * split_line - split a line into tokens
@@ -6,37 +8,41 @@
  *
  * Return: Null-terminated array of tokens
  */
-char **split_line(char *line)
+
+/**
+   @brief Split a line into tokens (very naively).
+   @param line The line.
+   @return Null-terminated array of tokens.
+ */
+char **lsh_split_line(char *line)
 {
-	int bufsize = LSH_TOK_BUFSIZE, position = 0;
-	char **tokens = malloc(bufsize * sizeof(char*));
-	char *token, **tokens_backup;
+  int bufsize = LSH_TOK_BUFSIZE, position = 0;
+  char **tokens = malloc(bufsize * sizeof(char*));
+  char *token, **tokens_backup;
 
-	if (!tokens)
-	{
-		fprintf(stderr, "allocation error\n");
-		exit(EXIT_FAILURE);
-	}
-	token = strtok(line, LSH_TOK_DELIM);
-	while (token != NULL)
-	{
-		tokens[position] = token;
-		position++;
+  if (!tokens) {
+    fprintf(stderr, "lsh: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
 
-		if (position >= bufsize)
-		{
-			bufsize += LSH_TOK_BUFSIZE;
-			tokens_backup = tokens;
-			tokens = realloc(tokens, bufsize * sizeof(char*));
-			if (!tokens)
-			{
-				free(tokens_backup);
-				fprintf(stderr, "allocation error\n");
-				exit(EXIT_FAILURE);
-			}
-		}
-		token = strtok(NULL, LSH_TOK_DELIM);
-	}
-	tokens[position] = NULL;
-	return (tokens);
+  token = strtok(line, LSH_TOK_DELIM);
+  while (token != NULL) {
+    tokens[position] = token;
+    position++;
+
+    if (position >= bufsize) {
+      bufsize += LSH_TOK_BUFSIZE;
+      tokens_backup = tokens;
+      tokens = realloc(tokens, bufsize * sizeof(char*));
+      if (!tokens) {
+		free(tokens_backup);
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    token = strtok(NULL, LSH_TOK_DELIM);
+  }
+  tokens[position] = NULL;
+  return tokens;
 }
