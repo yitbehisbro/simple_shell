d9b92505a5d0b422fe9739894a01dfc2917de61d
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-08-31 12:45:04 +0300

    Updates

--- main.c
+++ main.c
@@ -1,19 +1,14 @@
 #include "main.h"
 
 /**
-   @brief Main entry point.
-   @param argc Argument count.
-   @param argv Argument vector.
-   @return status code
+ * main - entry point.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: status code
  */
 int main(int __attribute__((unused)) argc, char __attribute__((unused)) **argv)
 {
-  // Load config files, if any.
-
-  // Run command loop.
-  lsh_loop();
-
-  // Perform any shutdown/cleanup.
-
-  return EXIT_SUCCESS;
+	hsh_loop();
+	return (EXIT_SUCCESS);
 }
