92cd2f1a5ed54011ae6266c13f0d2ea5c1c186d1
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-08-31 12:58:49 +0300

    Updates

--- builtin.c
+++ builtin.c
@@ -16,25 +16,26 @@ int hsh_num_builtins(void)
 	return (sizeof(builtin_str1) / sizeof(char *));
 }
 
-/*
-  Builtin function implementations.
-*/
-
 /**
-   @brief Builtin command: change directory.
-   @param args List of args.  args[0] is "cd".  args[1] is the directory.
-   @return Always returns 1, to continue executing.
+ * hsh_cd - change directory.
+ * @args: List of arguments.
+ *
+ * Return: Always 1
  */
-int lsh_cd(char **args)
+int hsh_cd(char **args)
 {
-  if (args[1] == NULL) {
-    fprintf(stderr, "lsh: expected argument to \"cd\"\n");
-  } else {
-    if (chdir(args[1]) != 0) {
-      perror("lsh");
-    }
-  }
-  return 1;
+	if (args[1] == NULL)
+	{
+		fprintf(stderr, "expected argument to \"cd\"\n");
+	}
+	else
+	{
+		if (chdir(args[1]) != 0)
+		{
+			perror("lsh");
+		}
+	}
+	return (1);
 }
 
 /**
