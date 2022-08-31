e707ebc3685b0c4384adee59f126544f872dbc56
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-08-31 14:33:57 +0300

    Updates

--- launch.c
+++ launch.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
  * hsh_launch - launchs a program
@@ -9,29 +8,28 @@
  */
 int hsh_launch(char **args)
 {
-  pid_t pid;
-  int status;
+	pid_t pid;
+	int status;
 
-  pid = fork();
-  if (pid == 0) {
-    // Child process
-    if (execve(args[0], args, NULL) == -1) {
-      //perror(args[0]);
-      //perror(args[2]);
-      fprintf(stderr, "not found\n");
-      //exit(EXIT_FAILURE);
-    }
-    //exit(EXIT_FAILURE);
-  } else if (pid < 0) {
-    // Error forking
-    //perror("lsh");
-    //perror(args[0]);
-    fprintf(stderr, "not found\n");
-  } else {
-    // Parent process
-    do {
-      waitpid(pid, &status, WUNTRACED);
-    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-  }
-  return (1);
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execvp(args[0], args) == -1)
+		{
+			fprintf(stderr, "%s: not found\n", args[0]);
+		}
+		exit(EXIT_FAILURE);
+	}
+	else if (pid < 0)
+	{
+		fprintf(stderr, "not found\n");
+	}
+	else
+	{
+		do
+		{
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	return (1);
 }
