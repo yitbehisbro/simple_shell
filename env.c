05f327aa5992e7c2c6c1533afbd55121ebbe85b3
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-04 01:21:59 +0300

    betty style fixes

--- env.c
+++ env.c
@@ -11,20 +11,20 @@ int _env(void)
 	{
 		fprintf(stderr, "Error: C_IS not setted\n");
 	}
-        printf("USER=%s\n", getenv("USER"));
-        printf("LANGUAGE=%s\n", getenv("LANG"));
-        printf("SESSION=%s\n", getenv("SESSION"));
-        printf("COMPIZ_CONFIG_PROFILE=%s\n", getenv("COMPIZ_CONFIG_PROFILE"));
-        printf("SHLVL=%s\n", getenv("SHLVL"));
-        printf("HOME=%s\n", getenv("HOME"));
-        printf("C_IS=%s\n", getenv("C_IS"));
-        printf("DESKTOP_SESSION=%s\n", getenv("DESKTOP_SESSION"));
-        printf("LOGNAME=%s\n", getenv("LOGNAME"));
-        printf("TERM=%s\n", getenv("TERM"));
-        printf("PATH=%s\n", getenv("PATH"));
-        printf("DISPLAY=%s\n", getenv("DISPLAY"));
+	printf("USER=%s\n", getenv("USER"));
+	printf("LANGUAGE=%s\n", getenv("LANG"));
+	printf("SESSION=%s\n", getenv("SESSION"));
+	printf("COMPIZ_CONFIG_PROFILE=%s\n", getenv("COMPIZ_CONFIG_PROFILE"));
+	printf("SHLVL=%s\n", getenv("SHLVL"));
+	printf("HOME=%s\n", getenv("HOME"));
+	printf("C_IS=%s\n", getenv("C_IS"));
+	printf("DESKTOP_SESSION=%s\n", getenv("DESKTOP_SESSION"));
+	printf("LOGNAME=%s\n", getenv("LOGNAME"));
+	printf("TERM=%s\n", getenv("TERM"));
+	printf("PATH=%s\n", getenv("PATH"));
+	printf("DISPLAY=%s\n", getenv("DISPLAY"));
 
-        return (0);
+	return (0);
 }
 /**
  * hsh_env - list the env variables value
