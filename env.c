825605f9063fbe3f079f6e6b7de56a65e9cc70df
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-03 15:26:01 +0300

    Created

---
+++ env.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _env - prints the result for env
+ *
+ * Result: Always 0
+ */
+int _env(void)
+{
+	/**
+	 * char *home = getenv("HOME");
+         * char *ses = getenv("SESSION");
+         * char *lang = getenv("LANGUAGE");
+         * char *ptr = getenv("USER");
+         * char *shl = getenv("SHLVL");
+         * char *cnf = getenv("COMPIZ_CONFIG_PROFILE");
+         * char *dses = getenv("DESKTOP_SESSION");
+         * char *log = getenv("LOGNAME");
+         * char *term = getenv("TERM");
+         * char *path = getenv("PATH");
+         * char *dis = getenv("DISPLAY");
+	*/
+        printf("USER=%s\n", getenv("USER"));
+        printf("LANGUAGE=%s\n", getenv("LANGUAGE"));
+        printf("SESSION=%s\n", getenv("SESSION"));
+        printf("COMPIZ_CONFIG_PROFILE=%s\n", getenv("COMPIZ_CONFIG_PROFILE"));
+        printf("SHLVL: %s\n", getenv("SHLVL"));
+        printf("HOME=%s\n", getenv("HOME"));
+        printf("C_IS=Fun_:)\n");
+        printf("DESKTOP_SESSION=%s\n", getenv("DESKTOP_SESSION"));
+        printf("LOGNAME=%s\n", getenv("LOGNAME"));
+        printf("TERM=%s\n", getenv("TERM"));
+        printf("PATH=%s\n", getenv("PATH"));
+        printf("DISPLAY=%s\n", getenv("DISPLAY"));
+
+        return (0);
+}
