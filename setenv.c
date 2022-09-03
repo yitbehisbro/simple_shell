74da847cb10be2945782f21e3652b087a894e408
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-03 20:02:16 +0300

    Created

---
+++ setenv.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _setenv - set envirnomental variable
+ * @args: arguments vector
+ *
+ * Return: -1 in error and 1 to enable prompt
+ */
+int _setenv(char **args)
+{
+	if (setenv(args[1], args[2], 1) == -1)
+	{
+		fprintf(stderr, "Failed to set the variable"); 
+	}
+	return (1);
+}
+
+/**
+ * _unsetenv - set envirnomental variable
+ * @args: arguments vector
+ *
+ * Return: -1 in error, 0 for success and 1 to enable prompt
+ */
+int _unsetenv(char **args)
+{
+	if (unsetenv(args[1]) == -1)
+	{
+		fprintf(stderr, "Failed to unset the variable"); 
+	}
+	return (1);
+}
+	
