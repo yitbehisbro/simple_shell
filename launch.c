3491634348f00de6e874a293a6a64dabc6730ac5
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-03 17:04:58 +0300

    Updates

--- launch.c
+++ launch.c
@@ -46,9 +46,9 @@ int _atoi(char *s)
  *
  * Return: exit status
  */
-int hsh_exit_status(char *args)
+int hsh_exit_status(char **args)
 {
-	int status = _atoi((char *)args[0]);
+	int status = _atoi(args[0]);
 
 	exit(status);
 }
@@ -69,7 +69,7 @@ int hsh_launch(char **args)
 	{
 		if (execvp(args[0], args) == -1)
 		{
-			if ((args[0] == exit_status[0]) && (_atoi(args[1]) >= 0) || (_atoi(args[1]) <= 0) && (args[2] == NULL))
+			if ((args[0] == exit_status[0] && _atoi(args[1]) >= 0) || (_atoi(args[1]) <= 0) && (args[2] == NULL))
 				hsh_exit_status(args[1]);
 			else
 				fprintf(stderr, "%s: not found\n", args[0]);
