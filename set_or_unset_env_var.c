56790e38ec604dd3179ef8057f4fe9cf942217c3
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-03 21:12:55 +0300

    Updates

--- set_or_unset_env_var.c
+++ set_or_unset_env_var.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * count_args - counts the parameter passed
+ * @argv: argument vector
+ *
+ * Return: number of word
+ */
+int count_args(char **argv)
+{
+	int counter = 1;
+
+	while (argv[i] != NULL)
+	{
+		if (argv[i] == " ")
+			counter++;
+		i++
+	}
+	return (counter);
+}
+/**
  * _setenv - set envirnomental variable
  * @args: arguments vector
  *
@@ -8,6 +26,10 @@
  */
 int _setenv(char **args)
 {
+	if (count_args(args) < 2 || count_args(args) > 3)
+	{
+		fprintf(stderr, "hsh: Failed to set the variable\n");
+	}	
 	if (args == NULL)
 	{
 		return (1);
