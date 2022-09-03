897453b52f9bd3114515b99a1f4b4057703af610
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-03 20:40:58 +0300

    Updates

--- launch.c
+++ launch.c
@@ -74,10 +74,10 @@ int hsh_launch(char **args)
 				hsh_exit_status(_atoi(args[1]));
 			else if ((args[0] == exit_status[0]) && (args[1] == NULL))
 				hsh_exit(args);
-			else if ((args[0] == setenv_var[0]) && (args[1] != NULL) && (args[2] != NULL) && (args[3] == NULL))
+			/** else if ((args[0] == setenv_var[0]) && (args[1] != NULL) && (args[2] != NULL) && (args[3] == NULL))
 				_setenv(args);
 			else if ((args[0] == unsetenv_var[0]) && (args[1] != NULL) && (args[2] == NULL))
-				_unsetenv(args);			
+				_unsetenv(args);*/	
 			else
 				fprintf(stderr, "%s: not found\n", args[0]);
 		}
