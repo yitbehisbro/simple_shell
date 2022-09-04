2ecc47c6efcf286f0e80830766002dfc6e00580f
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-04 14:35:11 +0300

    Updates

--- main.h
+++ main.h
@@ -1,8 +1,8 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#define LSH_TOK_BUFSIZE 64
-#define LSH_TOK_DELIM "\" \t\r\n\a"
+#define TOKEN_BUFSIZE 64
+#define TOKEN_DELIM "\" \t\r\n\a"
 
 #include <sys/wait.h>
 #include <sys/types.h>
