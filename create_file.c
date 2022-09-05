eab36e4282b5cea0440728badc5e9801fd11aa46
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-05 14:34:49 +0300

    Created

---
+++ create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file.
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure (file can not be created,
+ * file can not be written, write "fails", etc...)
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file_d, size = 0, buff;
+
+	if (filename == NULL)
+		return (-1);
+
+	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (file_d == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	/** to get the size of text_content */
+	while (*(text_content + size) != '\0')
+		size++;
+
+	buff = write(file_d, text_content, size);
+	if (buff == -1)
+		return (-1);
+
+	close(file_d);
+	return (1);
+}
