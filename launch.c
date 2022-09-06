5c61c9a1b547407435d579d9959f42107b08f1ac
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-06 15:17:29 +0300

    Updates

--- launch.c
+++ launch.c
@@ -67,7 +67,7 @@ int hsh_launch(char **args)
 	pid = fork();
 	if (pid == 0)
 	{
-		echo_argv(args, status, 0);
+		echo_argv(args, status);
 		if (execvp(args[0], args) == -1)
 		{
 			if (((args[0] == exit_status[0]) && (_atoi(args[1]) >= 0 ||
@@ -93,7 +93,7 @@ int hsh_launch(char **args)
 		do {
 			waitpid(pid, &status, WUNTRACED);
 		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
-		echo_argv(argv_exit, op_exit_status(status), 1);
+		echo_argv( , op_exit_status(status));
 	}
 	return (1);
 }
