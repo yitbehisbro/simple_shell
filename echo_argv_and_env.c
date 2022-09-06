c73aff75409fbc36bd94611f193888446abe8af9
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-06 15:00:56 +0300

    Updates

--- echo_argv_and_env.c
+++ echo_argv_and_env.c
@@ -59,3 +59,35 @@ int op_exit_status(int status)
 	exit_status = WEXITSTATUS(status);
 	return (exit_status);
 }
+
+/**
+ * echo_argv_print - prints the argument vectors
+ * @argv: argument vector
+ * @status: exit status
+ *
+ * Return: 1 in success else -1
+ */
+int echo_argv_print(char **argv, int status)
+{
+	pid_t pid;
+	char *ch[] = {"echo", "$$", "$?", "$PATH", NULL};
+
+	pid = getppid();
+	if (argv == NULL)
+		return (-1);
+
+	if (strcmp(argv[0], ch[0]) == 0)
+	{
+		if (strcmp(argv[1], ch[2]) == 0)
+		{
+			printf("%d\n", status);
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			perror("sh");
+			exit(EXIT_FAILURE);
+		}
+	}
+	return (1);
+}
