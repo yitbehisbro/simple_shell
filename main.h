ae06de37834db5feb6b17e26a03bec01c59d7558
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-06 15:41:10 +0300

    Updates

--- main.h
+++ main.h
@@ -42,7 +42,7 @@ int _setenv(char **args);
 int count_args(char **argv);
 int create_file(const char *filename, char *text_content);
 char *read_file(const char *filename, size_t letters);
-int echo_argv(char **argv, int status);
+int echo_argv(char **argv);
 int op_exit_status(int status);
 
 #endif
