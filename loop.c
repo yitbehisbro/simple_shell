d2c7caf1452cf3efa21d7f02e164c873887bf815
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-07 11:19:10 +0300

    Updates

--- loop.c
+++ loop.c
@@ -1,23 +1,77 @@
 #include "main.h"
 
 /**
- * hsh_loop - read input until exit
+ * without_comment - deletes comments from the input
  *
- * Return: void no return
+ * @in: input string
+ * Return: input without comments
  */
-void hsh_loop(void)
+char *without_comment(char *in)
 {
-	char *line;
-	char **args;
-	int status;
+	int i, up_to;
 
-	do {
-		printf("#cisfun$ ");
-		line = read_line();
-		args = split_line(line);
-		status = hsh_execute(args);
+	up_to = 0;
+	for (i = 0; in[i]; i++)
+	{
+		if (in[i] == '#')
+		{
+			if (i == 0)
+			{
+				free(in);
+				return (NULL);
+			}
 
-		free(line);
-		free(args);
-	} while (status);
+			if (in[i - 1] == ' ' || in[i - 1] == '\t' || in[i - 1] == ';')
+				up_to = i;
+		}
+	}
+
+	if (up_to != 0)
+	{
+		in = _realloc(in, i, up_to + 1);
+		in[up_to] = '\0';
+	}
+
+	return (in);
+}
+
+/**
+ * shell_loop - Loop of shell
+ * @datash: data relevant (av, input, args)
+ *
+ * Return: no return.
+ */
+void shell_loop(data_shell *datash)
+{
+	int loop, i_eof;
+	char *input;
+
+	loop = 1;
+	while (loop == 1)
+	{
+		write(STDIN_FILENO, "^-^ ", 4);
+		input = read_line(&i_eof);
+		if (i_eof != -1)
+		{
+			input = without_comment(input);
+			if (input == NULL)
+				continue;
+
+			if (check_syntax_error(datash, input) == 1)
+			{
+				datash->status = 2;
+				free(input);
+				continue;
+			}
+			input = rep_var(input, datash);
+			loop = split_commands(datash, input);
+			datash->counter += 1;
+			free(input);
+		}
+		else
+		{
+			loop = 0;
+			free(input);
+		}
+	}
 }
