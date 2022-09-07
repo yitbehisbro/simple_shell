2f822a6ab14045257a4119552885d34b5bb9ad4f
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-07 10:56:06 +0300

    Updates

--- execute.c
+++ execute.c
@@ -1,30 +1,6 @@
 #include "main.h"
 
 /**
- * _strcmp - compare two strings
- * @s1: pointer to character var 1
- * @s2: pointer to character var 2
- * Return: Always 0.
- */
-int _strcmp(char *s1, char *s2)
-{
-	while (*s1 == *s2 && (*s1 != '\0' && *s2 != '\0'))
-	{
-		s1++;
-		s2++;
-	}
-
-	if (*s1 != *s2)
-	{
-		return (*s1 - *s2);
-	}
-	else
-	{
-		return (0);
-	}
-}
-
-/**
  * hsh_execute - execute built-in command or launch program
  * @args: NULL terminated list of arguments
  *
