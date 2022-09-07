710d169b8aa621f6c78f033ca6c7d1f49a8b8efd
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-07 10:43:59 +0300

    Created

---
+++ shell_0_1_main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - entry point.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: status code
+ */
+int main(int __attribute__((unused)) argc, char __attribute__((unused)) **argv)
+{
+	shell_loop();
+	return (EXIT_SUCCESS);
+}
