59c4589dc357805c46268f87cfcbe719eeb2754c
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-07 11:26:07 +0300

    Updates

--- _getline.c
+++ _getline.c
@@ -1,108 +1,84 @@
 #include "main.h"
 
 /**
- * _strchr - search the first occernance of c
- * @s: pointer to char string
- * @c: character to be find
- * Return: pointer to s or NULL
+ * bring_line - assigns the line var for get_line
+ * @lineptr: Buffer that store the input str
+ * @buffer: str that is been called to line
+ * @n: size of line
+ * @j: size of buffer
  */
-char *_strchr(char *s, char c)
+void bring_line(char **lineptr, size_t *n, char *buffer, size_t j)
 {
-	unsigned int a = 0;
 
-	for (; *(s + a) != '\0'; a++)
-		if (*(s + a) == c)
-			return (s + a);
-	if (*(s + a) == c)
-		return (s + a);
-	return ('\0');
-}
-
-/**
- * _strlen - prints the length of string
- * @s: variable that holds string values
- *
- * Return: the length of the given string
- */
-int _strlen(char *s)
-{
-	int length;
-
-	length = 0;
-
-	while (*(s + length) != '\0')
+	if (*lineptr == NULL)
 	{
-		length++;
+		if  (j > BUFSIZE)
+			*n = j;
+
+		else
+			*n = BUFSIZE;
+		*lineptr = buffer;
 	}
-	return (length);
-}
-
-/**
- * _strcpy - copies the string pointed to by src
- * @dest: variable that holds the value of variable
- * @src: sting values
- * Return: Always 0.
- */
-char *_strcpy(char *dest, char *src)
-{
-	int length;
-
-	length = 0;
-
-	while (length >= 0)
+	else if (*n < j)
 	{
-		*(dest + length) = *(src + length);
-		if (*(src + length) == '\0')
-			break;
-		length++;
+		if (j > BUFSIZE)
+			*n = j;
+		else
+			*n = BUFSIZE;
+		*lineptr = buffer;
 	}
-	return (dest);
-}
-
-/**
- * _getline - reads an entire line from stream
- * @lineptr: pointer to the line
- * @stream: files to be read
- * @n: size of buffer
- *
- * Return: the number of characters read,
- * including the delimiter character, but not
- * including the terminating null byte
- */
-ssize_t _getline(char **lineptr, ssize_t *n, FILE *stream)
-{
-	static char line[256];
-	char *ptr;
-	unsigned int len;
-
-	if (lineptr == NULL || n == NULL)
+	else
 	{
-		errno = EINVAL;
-		return (-1);
+		_strcpy(*lineptr, buffer);
+		free(buffer);
 	}
+}
+/**
+ * get_line - Read inpt from stream
+ * @lineptr: buffer that stores the input
+ * @n: size of lineptr
+ * @stream: stream to read from
+ * Return: The number of bytes
+ */
+ssize_t get_line(char **lineptr, size_t *n, FILE *stream)
+{
+	int i;
+	static ssize_t input;
+	ssize_t retval;
+	char *buffer;
+	char t = 'z';
 
-	if (ferror(stream))
+	if (input == 0)
+		fflush(stream);
+	else
 		return (-1);
+	input = 0;
 
-	if (feof(stream))
+	buffer = malloc(sizeof(char) * BUFSIZE);
+	if (buffer == 0)
 		return (-1);
-
-	fgets(line, 256, stream);
-
-	ptr = _strchr(line, '\n');
-	if (ptr)
-		*ptr = '\0';
-
-	len = _strlen(line);
-
-	if ((len + 1) < 256)
+	while (t != '\n')
 	{
-		ptr = realloc(*lineptr, 256);
-		if (ptr == NULL)
+		i = read(STDIN_FILENO, &t, 1);
+		if (i == -1 || (i == 0 && input == 0))
+		{
+			free(buffer);
 			return (-1);
-		*lineptr = ptr;
-		*n = 256;
+		}
+		if (i == 0 && input != 0)
+		{
+			input++;
+			break;
+		}
+		if (input >= BUFSIZE)
+			buffer = _realloc(buffer, input, input + 1);
+		buffer[input] = t;
+		input++;
 	}
-	_strcpy(*lineptr, line);
-	return (len);
+	buffer[input] = '\0';
+	bring_line(lineptr, n, buffer, input);
+	retval = input;
+	if (i != 0)
+		input = 0;
+	return (retval);
 }
