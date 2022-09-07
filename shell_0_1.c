fdac88704d95cb3a08bb63e35001d2b593e885bf
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-07 10:34:31 +0300

    Updates

--- shell_0_1.c
+++ shell_0_1.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int hsh_launch(char **args)
+int shell_launch(char **args)
 {
 	pid_t pid;
 	int status;
@@ -26,4 +26,96 @@ int hsh_launch(char **args)
 		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
 	}
 	return (1);
+}
+
+/**
+ * shell_loop - read input until exit
+ *
+ * Return: void no return
+ */
+void shell_loop(void)
+{
+	char *line;
+	char **args;
+	int status;
+
+	do {
+		printf("#cisfun$ ");
+		line = read_shell_line();
+		args = split_line(line);
+		status = hsh_execute(args);
+
+		free(line);
+		free(args);
+	} while (status);
+}
+/**
+ * read_shell_line - read a line
+ *
+ * Return: Null-terminated
+ */
+char *read_shell_line(void)
+{
+	char *line = NULL;
+	ssize_t bufsize = 0;
+
+	if (_getline(&line, &bufsize, stdin) == -1)
+	{
+		if (feof(stdin))
+		{
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			perror("getline\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+	return (line);
+	return (read_shell_loop());
+}
+
+/**
+ * read_shell_loop - reads the line
+ *
+ * Return: buffer
+ */
+char *read_shell_loop(void)
+{
+	int c, position = 0, bufsize = BUFSIZE;
+	char *buffer = malloc(sizeof(char) * bufsize);
+
+	if (!buffer)
+	{
+		fprintf(stderr, "allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+	while (1)
+	{
+		c = getchar();
+		if (c == EOF)
+		{
+			exit(EXIT_SUCCESS);
+		}
+		else if (c == '\n')
+		{
+			buffer[position] = '\0';
+			return (buffer);
+		}
+		else
+		{
+			buffer[position] = c;
+		}
+		position++;
+		if (position >= bufsize)
+		{
+			bufsize += BUFSIZE;
+			buffer = realloc(buffer, bufsize);
+			if (!buffer)
+			{
+				fprintf(stderr, "allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
 }
