8f5aa68e6baa867718fd0a71a662388c0df517bb
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2022-09-07 14:03:45 +0300

    Updates

--- env_helper.c
+++ env_helper.c
@@ -51,9 +51,9 @@ void aux_help_general(void)
 	char *help = ":) bash, version 1.0 Copyright © 2022\n";
 
 	write(STDOUT_FILENO, help, _strlen(help));
-	help = "These commands are builtin; please type 'help' for other commands";
+	help = "These commands are builtin; please";
 	write(STDOUT_FILENO, help, _strlen(help));
-	help = "Type 'help name' to find out more about the function 'name'.\n\n ";
+	help = "type 'help name' to find out more about the function 'name'.\n\n ";
 	write(STDOUT_FILENO, help, _strlen(help));
 	help = "alias: alias [name=['string']]\n cd: cd [-L|[-P [-e]] [-@]] ";
 	write(STDOUT_FILENO, help, _strlen(help));
